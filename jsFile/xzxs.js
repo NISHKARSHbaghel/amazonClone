2a333b624e6d15f3b276fff0cf673a5c868760a9
Nishkarsh Baghel <nishkarshbaghel086@gmail.com>
2025-02-20 23:23:53 +0530

    added all the files

---
+++ jsFile/xzxs.js
@@ -0,0 +1,38 @@
+// function func1(){
+//     return 2;
+// }
+
+// function func2() {
+//     return 4;
+// }
+
+// let a = (func1(), func2())
+
+// console.log(a);
+
+
+
+const arr =['one', 'two' , 'three'];
+const str = 'hello';
+
+const res = arr.includes('on');
+const anotherVar = arr.includes('onetwo');
+const newVar = str.includes('ll');
+
+console.log(res);
+console.log(anotherVar);
+console.log(newVar);
+
+function abc() {
+    console.log(abc.xyz);    
+}
+
+abc
+abc.xyz = 400
+abc.xys = 200
+abc()
+
+
+const number = [1,2,3,4]
+number[100] = 500
+console.log(number);
